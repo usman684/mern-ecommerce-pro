0865426124227d8a843f0f3201c222c0c4387478
Muhammad Usman <usman.rauf.953@gmail.com>
2026-07-24 02:22:30 +0500

    server update

--- server/server.js
+++ server/server.js
@@ -36,6 +36,11 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is Running on PORT: ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is Running on PORT: ${PORT}`);
+  });
+}
+
+export default app;
